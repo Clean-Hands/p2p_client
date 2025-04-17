8a2d7e78e2401b253244ac63d269b53a76665675
Lazuli <34384401+Clean-Hands@users.noreply.github.com>
2025-04-17 17:47:28 -0500

    Client-client socket opening and communication with multithreading

--- p2p_client/src/main.rs
+++ p2p_client/src/main.rs
@@ -1,3 +1,143 @@
+// main.rs
+// by Ruben Boero, Lazuli Kleinhans
+// April 17th, 2025
+// CS347 Advanced Software Design
+
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::io::Read;
+use std::io::{self, Write};
+use std::thread;
+use std::env::args;
+use std::process;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub fn run_client_server(send_addr: &String, port: &String) {
+
+    // let send_addrs: [String; 2] = ["10.133.90.57:7878".to_string(), "10.133.90.57:7878".to_string()];
+    
+    // // spawn threads that open sending sockets
+    // for ip in send_addrs {
+    //     spawn_sender_thread(ip);
+    // }
+
+    spawn_sender_thread(send_addr.clone(), port.clone());
+
+    // handle incoming data and print it to the terminal
+    println!("Starting listener...");
+
+    let listen_addr = String::from("0.0.0.0:") + port;
+
+    let listener = match TcpListener::bind(&listen_addr) {
+        Ok(l) => {
+            println!("Server listening on {}", &listen_addr);
+            l
+        }
+        Err(e) => {
+            eprintln!("Failed to bind: {}", e);
+            return;
+        }
+    };
+
+    println!("Successfully started listener.");
+
+    for stream in listener.incoming() {
+        // check to see if listener.incoming returned an error
+        let mut stream = match stream {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("Failed to accept connection: {}", e);
+                continue;
+            }
+        };
+
+        // create an array of 512 bytes of 0s
+        let mut buffer: [u8; 512] = [0; 512];
+
+        loop {
+            // get the number of bytes read
+            // read() needs a fixed size input to write into
+            let num_bytes_read = match stream.read(&mut buffer) {
+                Ok(0) => { // if 0 bytes read
+                    println!("Partner disconnected");
+                    break;
+                }
+                Ok(n) => n,
+                Err(e) => {
+                    eprintln!("Failed to read from stream: {}", e);
+                    break;
+                }
+            };
+
+            // convert received bytes to String to print
+            // only read in the first num_bytes_read number of bytes
+            // e.g. if message is 10 bytes long, only convert those 10 bytes, not full 512
+            let received = String::from_utf8_lossy(&buffer[..num_bytes_read]);
+
+            println!(">>> {}", received.trim());
+        }
+    }
+}
+
+fn spawn_sender_thread(send_ip: String, port: String) {
+    thread::spawn(move || {
+        let mut stream: TcpStream;
+
+        let send_addr: String = send_ip + ":" + &port;
+        
+        // loop until connection is successful
+        loop {
+            println!("Attempting to start sender thread...");
+            match TcpStream::connect(&send_addr) {
+                Ok(s) => {
+                    println!("Sender connected to {}", &send_addr);
+                    stream = s;
+                    break;
+                },
+                Err(e) => {
+                    eprintln!("Failed to connect: {}", e);
+                    sleep(Duration::from_secs(1));
+                }
+            };
+        }
+
+        println!("Successfully started sender thread.");
+        
+        loop {
+            let mut message = String::new();
+
+            // if let tries to match the output of read_line to Err, if it does match, it prints error message,
+            // but if there is no error (Ok returned from read_line), then nothing happens
+            if let Err(e) = io::stdin().read_line(&mut message) {
+                eprintln!("Failed to read line: {}", e);
+                return;
+            }
+
+            if message.trim() == String::from("/exit"){
+                println!("Goodbye!");
+                process::exit(0);
+            }
+
+            match stream.write_all(message.as_bytes()) {
+                Ok(_) => continue,
+                Err(e) => {
+                    eprintln!("Failed to write to stream: {}", e);
+                    return;
+                }
+            }
+        }
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    // put all the command line arguments into a vector
+    let args: Vec<String> = args().collect();
+    
+    if args.len() < 4 {
+        eprintln!("Please specify an IP address to connect to along with a port.\nExample: cargo run p2p_client [IP address] [port number]");
+        process::exit(1);  // exit with error code 1 (common failure)
+    }
+
+    run_client_server(&args[2], &args[3]);
 }
