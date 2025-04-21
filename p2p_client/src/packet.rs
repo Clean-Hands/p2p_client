cdd5c24761edcc5be3d7f2adaa1d798119905694
Liam Keane <keanel@carleton.edu>
2025-04-21 17:38:07 -0500

    worked on serializing data as bytes

--- p2p_client/src/packet.rs
+++ p2p_client/src/packet.rs
@@ -1,8 +1,10 @@
+use std::mem;
 use sha256::{digest, try_digest};
 
+// packets are always 512 bytes long 
 struct Packet {
-    packet_length: u32,
-    filename_len: u32,
+    data_length: u16,
+    filename_len: u16,
     filename: &str,
     data: Vec<u8>,
     chunk_hash: [u8; 32],
@@ -10,13 +12,38 @@ struct Packet {
 }
 
 impl Packet {
-    /// wrap data
-    fn encode_packet(data: Vec<u8>) -> Packet {
-        // 
-    }
-
     /// extract data from packet and verify integrity
-    fn decode_packet(packet: Packet) -> Vec<u8> {
+    fn decode_packet(&self) -> Packet {
 
     }
+}
+
+/// wrap data
+// unsure that data should be type Vec
+fn encode_packet(filename: String, data: Vec<u8>) -> [u8; 512] {
+    // initialize packet array and offset
+    let packet: [u8; 512];
+    let offset = 0;
+    
+    // append data_length
+    let data_length = (data.len() as u16).to_be_bytes();
+    packet[offset..offset + mem::size_of::<u16>()].copy_from_slice(&data_length);
+    offset += mem::size_of::<u16>();
+
+    //
+
+}
+
+fn main() {
+    // initialize packet array and offset
+    let data: Vec<u8> = vec![1, 2, 3, 4];
+    let mut packet = [7u8; 10];
+    let mut offset = 0;
+    
+    // append data_length
+    let data_length = (data.len() as u16).to_be_bytes();
+    packet[offset..offset + mem::size_of::<u16>()].copy_from_slice(&data_length);
+    offset += mem::size_of::<u16>();
+
+    println!("{:?}", packet);
 }
