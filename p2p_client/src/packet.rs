34e5018ae736ab09381699d1eacd85610ab20554
Liam Keane <keanel@carleton.edu>
2025-04-24 18:42:16 -0500

    wrote hashing function + cleanup

--- p2p_client/src/packet.rs
+++ p2p_client/src/packet.rs
@@ -10,12 +10,25 @@ use byteorder::{ByteOrder, BigEndian};
 // packets are always 512 bytes long 
 #[derive(Default, Debug)]
 pub struct Packet {
-    data_length: u16,
+    data_length: u16,   // the sum of all bytes in packet EXCEPT padding bytes
     filename_len: u16,
     filename: String,
     data: Vec<u8>,
-    chunk_hash: [u8; 32],
-    file_hash: [u8; 32],
+    chunk_hash: [u8; 32],   // chunk hash computed over data_length, filename_len, filename, and data fields
+    file_hash: [u8; 32],    // using sha256 hashes always results in 32 bytes
+}
+
+/// given a vector of bytes, compute and return the sha256 hash
+fn compute_sha256_hash(data: Vec<u8>) -> [u8; 32]{
+    let mut hasher = Sha256::new();
+    hasher.update(&data[..]);
+
+    // read hash digest and consume hasher
+    let chunk_hash = hasher.finalize();
+    let mut output: [u8; 32] = [0; 32];
+    output.copy_from_slice(&chunk_hash);
+
+    output  // return hash
 }
 
 /// extract data from packet and verify integrity
@@ -55,6 +68,9 @@ pub fn decode_packet(packet_bytes: [u8; 512]) -> Packet {
     packet.chunk_hash = chunk_hash_arr;
     offset += mem::size_of::<u8>()*32; // 32 is len of sha256 hash
 
+    
+    // assert_eq!();
+
     // add file hash
     let mut file_hash_arr: [u8; 32] = [0; 32];
     file_hash_arr.copy_from_slice(&packet_bytes[offset..offset + 32]);
@@ -65,11 +81,12 @@ pub fn decode_packet(packet_bytes: [u8; 512]) -> Packet {
 }
 
 /// wrap data
-// unsure that data should be type Vec
+// ensure that data should be type Vec
 pub fn encode_packet(filename: String, data: Vec<u8>, file_hash: [u8; 32]) -> [u8; 512] {
     // initialize packet array and offset
     let mut packet: [u8; 512] = [0; 512];
     let mut offset = 0;
+    let mut hash_vec: Vec<u8> = vec![];
     
     // append data length
     let data_length: u16 = (mem::size_of::<u16>()*2 +
@@ -77,36 +94,33 @@ pub fn encode_packet(filename: String, data: Vec<u8>, file_hash: [u8; 32]) -> [u
                                 (data.len() as usize) + 
                                 (mem::size_of::<u8>() as usize)*32*2) as u16;
     let data_length_bytes: [u8; 2] = data_length.to_be_bytes();
-
     packet[offset..offset + mem::size_of::<u16>()].copy_from_slice(&data_length_bytes);
     offset += mem::size_of::<u16>();
+    hash_vec.extend_from_slice(&data_length_bytes);
 
     // append filename length
     let filename_len = (filename.len() as u16).to_be_bytes();
     packet[offset..offset + mem::size_of::<u16>()].copy_from_slice(&filename_len);
     offset += mem::size_of::<u16>();
+    hash_vec.extend_from_slice(&filename_len);
 
     // append filename
     let filename = filename.as_bytes();
     packet[offset..offset + filename.len()].copy_from_slice(filename);
     offset += filename.len();
+    hash_vec.extend_from_slice(&filename);
 
     // append data
     packet[offset..offset + data.len()].copy_from_slice(&data);
     offset += data_length as usize;
+    hash_vec.extend_from_slice(&data);
 
-    // append hash of chunk
-    // create a Sha256 object
-    let mut hasher = Sha256::new();
-    // write input message
-    hasher.update(&data[..]);
-    // read hash digest and consume hasher
-    let chunk_hash = hasher.finalize();
-    let chunk_hash_len = chunk_hash.len();
-    packet[offset..offset + chunk_hash_len].copy_from_slice(&chunk_hash);
-    offset += chunk_hash_len;
+    // compute and append chunk hash
+    let chunk_hash = compute_sha256_hash(hash_vec);
+    packet[offset..offset + chunk_hash.len()].copy_from_slice(&chunk_hash);
+    offset += chunk_hash.len();
 
-    // append hash of file
+    // append file hash of file
     packet[offset..offset + file_hash.len()].copy_from_slice(&file_hash);
     
     return packet;
