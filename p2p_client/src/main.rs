b61a44548d28089bf3538d572a69cb898f99b093
Liam Keane <keanel@carleton.edu>
2025-05-02 14:42:41 -0500

    rough DH implementation

--- p2p_client/src/main.rs
+++ p2p_client/src/main.rs
@@ -1,5 +1,5 @@
 //! main.rs
-//! by Ruben Boero, Lazuli Kleinhans
+//! by Ruben Boero, Lazuli Kleinhans, Liam Keane
 //! April 22nd, 2025
 //! CS347 Advanced Software Design
 
@@ -9,6 +9,7 @@ use std::thread::{self, sleep};
 use std::time::Duration;
 use std::env::args;
 use std::process;
+use std::mem::replace;
 use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
 use aes_gcm::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
@@ -63,10 +64,10 @@ fn connect_sender_stream(send_ip: &String, port: &String) -> TcpStream {
 /// let message = String::from("Hello, world!");
 /// let stream: TcpStream = send_to_all_connections(&senders, message);
 /// ```
-fn send_to_all_connections(streams: &Vec<TcpStream>, message: String) {
+fn send_to_all_connections(streams: &mut Vec<ConnectionInfo>, message: String) {
 
-    for mut stream in streams {
-        if let Err(e) = stream.write_all(message.as_bytes()) {
+    for stream in streams {
+        if let Err(e) = stream.sender_stream.write_all(message.as_bytes()) {
             eprintln!("Failed to write to stream: {e}");
             return;
         }
@@ -92,22 +93,47 @@ fn start_sender_thread(send_addrs: Vec<String>, port: String, username: String) 
 
         for addr in &send_addrs {
             let sender_secret = EphemeralSecret::random_from_rng(&mut OsRng);
+            let stream = connect_sender_stream(addr, &port);
             let info = ConnectionInfo {
-                sender_stream: connect_sender_stream(addr, &port),
+                sender_stream: stream,
                 dh_public_key: PublicKey::from(&sender_secret),
                 dh_private_key: sender_secret,
                 dh_shared_secret: None
             };
             senders.push(info);
         }
-
+       
         // carry out DH exchange
-        for connection in &senders{
+        for connection in &mut senders {
             // send public key to listener
+            if let Err(e) = connection.sender_stream.write_all(connection.dh_public_key.as_bytes()) {
+                eprintln!("Failed to send DH public key: {e}");
+                return;
+            }
+
             // wait for public key response from listener
+            let mut public_key_bytes = [0u8; 32];
+            connection.sender_stream.read_exact(&mut public_key_bytes).expect("Failed to read peer's public key");
+            let peer_public_key = PublicKey::from(public_key_bytes);
+
             // compute and save shared secret in struct
+            // TODO: find out why we need to replace the shared secret within the struct
+            //       and get rid of the gross code below
+            let dh_private_key = std::mem::replace (
+                &mut connection.dh_private_key,
+                EphemeralSecret::random_from_rng(&mut OsRng), // replace with throwaway
+            );
+            connection.dh_shared_secret = Some(dh_private_key.diffie_hellman(&peer_public_key));
+            
+
+            // debug
+            println!("SENDER PUBLIC KEY {:?}:", public_key_bytes);
+            if let Some(secret) = &connection.dh_shared_secret {
+                println!("SHARED SECRET (sender): {:02x?}", secret.as_bytes());
+            }
         }
 
+        // commence encrypted messaging
         loop {
             let mut message = String::new();
             // if let tries to match the output of read_line to Err, if it does match, it prints error message,
@@ -120,13 +146,13 @@ fn start_sender_thread(send_addrs: Vec<String>, port: String, username: String) 
             if message.trim() == String::from("/exit"){
                 println!("Goodbye!");
                 // tell other users you disconnected
-                send_to_all_connections(&senders, format!("[{username} disconnected]"));
+                send_to_all_connections(&mut senders, format!("[{username} disconnected]"));
                 process::exit(0);
             }
 
             // send your message along with your username so others know who sent it
             message = format!("[{username}] {message}");
-            send_to_all_connections(&senders, message);
+            send_to_all_connections(&mut senders, message);
         }
     });
 }
@@ -176,6 +202,29 @@ fn run_client_server(send_addrs: &[String], port: String, username: String) {
     
         // create new thread for each incoming stream to handle more than one connection
         thread::spawn(move || {
+            // generate DH exchange info
+            let my_private_key = EphemeralSecret::random_from_rng(&mut OsRng);
+            let my_public_key = PublicKey::from(&my_private_key);
+
+            // read public key from sender
+            let mut public_key_bytes = [0u8; 32];
+            stream.read_exact(&mut public_key_bytes).expect("Failed to read peer's public key");
+            let peer_public_key = PublicKey::from(public_key_bytes);
+
+            // send our public key to sender
+            if let Err(e) = stream.write_all(my_public_key.as_bytes()) {
+                eprintln!("Failed to send my public key: {e}");
+                return;
+            }
+
+            //debug
+            println!("RECEIVER PUBLIC KEY {:?}:", public_key_bytes);
+
+            // generate shared secret
+            let shared_secret = my_private_key.diffie_hellman(&peer_public_key);
+            println!("RECEIVER SHARED SECRET {:02x?}:", shared_secret.as_bytes());
+
+            // read ciphertext and decrypt message into plaintext
             let mut buffer: [u8; 512] = [0; 512];
             loop {
                 let num_bytes_read = match stream.read(&mut buffer) {
