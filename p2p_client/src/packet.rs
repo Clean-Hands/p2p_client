e5e4864b9b5259048688adec7f5ca4bf024e044c
Ruben Boero <boeror2017@gmail.com>
2025-05-06 10:10:38 -0500

    Removed chunk hash from packet and updated tests

--- p2p_client/src/packet.rs
+++ p2p_client/src/packet.rs
@@ -7,19 +7,17 @@ use std::mem;
 use sha2::{Sha256, Digest};
 use byteorder::{ByteOrder, BigEndian};
 
+const PACKET_SIZE: usize = 512;
 
 /// Packet struct to contain relevant items of our packet protocol 
 /// 
 /// packets are always 512 bytes long (padded with 0s as needed)
 /// 
 /// data length: the sum of all bytes in packet EXCEPT padding bytes
-/// 
-/// chunk_hash: sha256 chunk hash computed over data_length and data fields
 #[derive(Default, Debug, PartialEq)]
 pub struct Packet {
     pub data_length: u16,
-    pub data: Vec<u8>, // up to 478 bytes
-    pub chunk_hash: [u8; 32],
+    pub data: Vec<u8>, // up to 510 bytes
 }
 
 /// given a vector of bytes, compute and return the sha256 hash 
@@ -35,77 +33,50 @@ pub fn compute_sha256_hash(data: &Vec<u8>) -> [u8; 32]{
     output  // return hash
 }
 
-/// extract data from packet and verify integrity via hash
+/// extract data from packet
 /// 
 /// returns Result type that contains Err if the chunk hash verification fails, and the created 
 /// Packet struct otherwise
-pub fn decode_packet(packet_bytes: [u8; 512]) -> Result<Packet, String> {
+pub fn decode_packet(packet_bytes: [u8; PACKET_SIZE]) -> Result<Packet, String> {
     let mut packet: Packet = Packet{..Default::default()};
-    let mut chunk_to_hash: Vec<u8> = vec![];
     let mut offset = 0;
 
     // convert data length bytes into u16
     let data_len = BigEndian::read_u16(&packet_bytes[offset..offset + mem::size_of::<u16>()]);
+
+    // check for malformed packet
+    if data_len > PACKET_SIZE as u16 {
+        return Err(format!("Data length of '{}' is larger than maximum packet size of '{}'", data_len, PACKET_SIZE));
+    }
+
     packet.data_length = data_len;
-    chunk_to_hash.extend_from_slice(&packet_bytes[offset..offset + mem::size_of::<u16>()]);
     offset += mem::size_of::<u16>();
 
-    // compute the length of file data
-    let file_data_len = data_len - 
-                    mem::size_of::<u16>() as u16 -  // subtract data length
-                    mem::size_of::<u8>() as u16*32; // subtract length of sha256 hash
-
     // add file data 
-    packet.data = packet_bytes[offset..offset + (file_data_len as usize)].to_vec();
-    chunk_to_hash.extend_from_slice(&packet_bytes[offset..offset + (file_data_len as usize)]);
-    offset += file_data_len as usize;
-
-    // add chunk hash
-    let mut chunk_hash_arr: [u8; 32] = [0; 32];
-    chunk_hash_arr.copy_from_slice(&packet_bytes[offset..offset + 32]);
-    packet.chunk_hash = chunk_hash_arr;
-
-    let chunk_hash = compute_sha256_hash(&chunk_to_hash);
-    
-    if chunk_hash != packet.chunk_hash {
-        return Err("Computed chunk hash does not match chunk hash within packet.".to_string());
-    }
+    let file_data_len = data_len - mem::size_of::<u16>() as u16;
+    packet.data = packet_bytes[offset..offset + file_data_len as usize].to_vec();
 
     Ok(packet) // return the completed packet
 }
 
 /// wrap data in our packet protocol
 /// 
-/// input filename: name of the file that is being sent
-/// 
 /// input data: bytes that represent the chunk of the file being sent
 /// 
-/// input file_hash: sha256 hash of the entire file being sent
-/// 
 /// output: array of bytes to send
-pub fn encode_packet(data: Vec<u8>) -> [u8; 512] {
+pub fn encode_packet(data: Vec<u8>) -> [u8; PACKET_SIZE] {
     // initialize packet array and offset
-    let mut packet: [u8; 512] = [0; 512];
+    let mut packet: [u8; PACKET_SIZE] = [0; PACKET_SIZE];
     let mut offset = 0;
-    let mut hash_vec: Vec<u8> = vec![];
     
     // append data length
-    let data_length: u16 = (mem::size_of::<u16>() +
-                                data.len() as usize + 
-                                (mem::size_of::<u8>() as usize)*32) as u16;
+    let data_length: u16 = (mem::size_of::<u16>() + data.len()) as u16;
     let data_length_bytes: [u8; 2] = data_length.to_be_bytes();
     packet[offset..offset + mem::size_of::<u16>()].copy_from_slice(&data_length_bytes);
-    hash_vec.extend_from_slice(&data_length_bytes);
     offset += mem::size_of::<u16>();
     
     // append data
     packet[offset..offset + data.len()].copy_from_slice(&data);
-    hash_vec.extend_from_slice(&data);
-    offset += data.len();
-    
-    // compute and append chunk hash
-    let chunk_hash = compute_sha256_hash(&hash_vec);
-    packet[offset..offset + chunk_hash.len()].copy_from_slice(&chunk_hash);
     
     return packet;
 }
@@ -114,15 +85,14 @@ pub fn encode_packet(data: Vec<u8>) -> [u8; 512] {
 // TODO: these tests aren't amazing since they assume that the output from functions are correct. 
 // The output is then hard coded in the tests. Not sure of better way to fix them. 
 mod tests {
-    // use super::*;
-    // why can i not use super like in line above?
-    use crate::packet::{self, Packet};
+    use super::*;
+    use crate::packet::{self, Packet, PACKET_SIZE};
 
     #[test]
     fn test_encode_packet() {
         let data = vec![1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
-        let actual: [u8; 512] = packet::encode_packet(data);
-        let expected: [u8; 512] = [0, 45, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 71, 32, 243, 31, 162, 211, 129, 246, 207, 35, 206, 186, 164, 176, 168, 165, 176, 142, 231, 133, 53, 190, 152, 159, 145, 197, 127, 235, 87, 208, 246, 62, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let actual: [u8; PACKET_SIZE] = packet::encode_packet(data);
+        let expected: [u8; PACKET_SIZE] = [0, 13, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         
         assert_eq!(actual, expected);
     }
@@ -130,17 +100,34 @@ mod tests {
     #[test]
     fn test_decode_packet() {
         let expected = Packet {
-            data_length: 45, 
+            data_length: 13, 
             data: vec![1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
-            chunk_hash: [71, 32, 243, 31, 162, 211, 129, 246, 207, 35, 206, 186, 164, 176, 168, 165, 176, 142, 231, 133, 53, 190, 152, 159, 145, 197, 127, 235, 87, 208, 246, 62],
         };
 
         let data = vec![1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
-        let packet: [u8; 512] = packet::encode_packet(data);
+        let packet: [u8; PACKET_SIZE] = packet::encode_packet(data);
         let actual = packet::decode_packet(packet);
 
         // need to wrap expected in Ok so that it matches the actual output, which hopefully is also Ok
         assert_eq!(actual, Ok(expected));
+    }
+
+    #[test]
+    fn test_decode_packet_error() {
+        // manually encode a malformed packet
+        let data_len: u16 = PACKET_SIZE as u16 + 1;  // data_len is larger than PACKET_SIZE
+        let mut packet_bytes: [u8; PACKET_SIZE] = [0; PACKET_SIZE];
+
+        BigEndian::write_u16(&mut packet_bytes[0..2], data_len);
+
+        let result = packet::decode_packet(packet_bytes);
+
+        let expected_error = format!(
+            "Data length of '{}' is larger than maximum packet size of '{}'",
+            data_len, PACKET_SIZE
+        );
+        
+        assert_eq!(result, Err(expected_error));
     }
 
     // I wasn't convinced that the PartialEq that rust autogenerated for Packet struct would be correct
@@ -149,26 +136,12 @@ mod tests {
         let expected = Packet {
             data_length: 87,
             data: vec![1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
-            chunk_hash: [225, 12, 171, 217, 101, 208, 53, 140, 202, 193, 162, 185, 202, 9, 198, 105, 184, 61, 132, 233, 44, 148, 213, 111, 38, 87, 245, 175, 76, 14, 186, 117],
         };
 
         let data = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-        let packet: [u8; 512] = packet::encode_packet(data);
+        let packet: [u8; PACKET_SIZE] = packet::encode_packet(data);
         let actual = packet::decode_packet(packet);
 
         assert_ne!(actual, Ok(expected));
-    }
-
-    #[test]
-    fn test_hash_check_failure() {
-        let data = vec![1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
-        let mut packet: [u8; 512] = packet::encode_packet(data);
-
-        let chunk_hash_start = 2 + 2 + 8 + 11; // header + filename + data
-        packet[chunk_hash_start] ^= 0xFF; // flip bits of chunk hash to cause error (using XOR)
-        let actual = packet::decode_packet(packet);
-        
-        assert!(actual.is_err());
-        assert_eq!(actual.unwrap_err(), "Computed chunk hash does not match chunk hash within packet.");
     }
 }
