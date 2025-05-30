373560b782d59f4215b0df17d04c8b107cfcdc12
Liam Keane <keanel@carleton.edu>
2025-05-30 11:35:58 -0500

    implemented test for file transfer

--- p2p_client/src/main.rs
+++ p2p_client/src/main.rs
@@ -150,3 +150,94 @@ fn main() {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::{
+        fs,
+        io::{BufWriter, Write},
+        path::PathBuf,
+        net::TcpListener
+    };
+    use tempfile::NamedTempFile;
+    use tokio::runtime::Runtime;
+    use crate::{listener, packet::compute_sha256_hash, requester};
+
+    /// create a temp file for testing
+    fn create_large_file(size_in_mb: usize) -> NamedTempFile {
+        let file = NamedTempFile::new().unwrap();
+        {
+            let mut writer = BufWriter::new(&file);
+            let buffer = vec![b'x'; 1024]; // 1 KB
+
+            for _ in 0..(size_in_mb * 1024) {
+                writer.write_all(&buffer).unwrap();
+            }
+            writer.flush().unwrap();
+        }
+        return file
+    }
+
+    async fn listen_for_one_connection() {
+        println!("Starting listener...");
+        let listen_addr = String::from("0.0.0.0:7878");
+        let listener = match TcpListener::bind(&listen_addr) {
+            Ok(l) => {
+                println!("Client listening on {}", &listen_addr);
+                l
+            },
+            Err(e) => {
+                eprintln!("Failed to bind: {}", e);
+                return;
+            }
+        };
+        println!("Successfully started listener");
+
+        // start handling incoming connections
+        let (stream, addr) = listener.accept().expect("Failed to accept connection");
+        println!("Client connected: {}", addr);
+        listener::start_sender_task(stream).await;
+
+    }
+
+#[test]
+
+    /// send a file between two peers and verify its integrity
+    fn test_send_file() {
+        // initialize file
+        let dummy_file = create_large_file(10);
+        let bytes = fs::read(dummy_file.path()).unwrap();
+        let file_hash = compute_sha256_hash(&bytes);
+
+        if let Err(e) = listener::add_file_to_catalog(&dummy_file.path().display().to_string()) {
+            panic!("Error adding file to catalog: {e}");
+        }
+
+        // start listener
+        let runtime = Runtime::new().expect("Failed to create a runtime");
+        let _ = runtime.enter();
+        runtime.spawn(listen_for_one_connection());
+
+        // request file
+        let address = "127.0.0.1".to_string();
+        let rq_file_hash = hex::encode(&file_hash);
+        requester::request_file(address, rq_file_hash.clone(), PathBuf::from("."));
+
+        // validate the received file
+        let file_name = dummy_file.path().file_name().unwrap().to_string_lossy().into_owned();
+        let file_path = PathBuf::from(".").join(&file_name);
+        let data = fs::read(&file_path).expect("Failed to read file");
+        let computed_hash = compute_sha256_hash(&data);
+
+        assert_eq!(computed_hash, file_hash);
+
+        // Cleanup
+        if let Err(e) = listener::remove_file_from_catalog(&rq_file_hash) {
+            eprintln!("Error removing file from catalog: {e}");
+        }
+        let _ = fs::remove_file(&file_path);
+        
+    }
+
+}
